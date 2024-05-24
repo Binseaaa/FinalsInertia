d173893522b8a7629546d9f473c4ceafe467fdd7
Vincy Deguma <vinsmar8888@gmail.com>
2024-05-24 15:44:15 +0800

    main

--- resources/js/Pages/students/Index.vue
+++ resources/js/Pages/students/Index.vue
@@ -8,27 +8,64 @@
 
 <template>
     <AuthenticatedLayout>
-        <Table>
-            <thead>
-                <tr>
-                    <th>Id Number</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Department</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="student in students" :key="student.id">
-                    <td>{{ student.id_num }}</td>
-                    <td>{{ student.name }}</td>
-                    <td>{{ student.email }}</td>
-                    <td>{{ student.department }}</td>
-                    <td>{{ student.status }}</td>
-                    <td>...</td>
-                </tr>
-            </tbody>
-        </Table>
+        <div class="relative overflow-x-auto sm:rounded-lg">
+            <div class="p-4">
+                <label for="table-search" class="sr-only">Search</label>
+                <div class="relative mt-1">
+                    <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                        </svg>
+                    </div>
+                    <input type="text" id="table-search" class="block pt-2 ps-10 text-sm border border-gray-300 rounded-lg w-80 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
+                </div>
+            </div>
+            <table class="w-[80vw] text-sm text-left rtl:text-right text-white p-5">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Id Number
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Name
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Email
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Department
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Status
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="student in students" :key="student.id" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ student.id_num }}
+                        </th>
+                        <td class="px-6 py-4">
+                            {{ student.name }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ student.email }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ student.department }}
+                        </td>
+                        <td class="px-6 py-4">
+                            {{ student.status }}
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                    </tbody>
+            </table>
+        </div>
     </AuthenticatedLayout>
 </template>
